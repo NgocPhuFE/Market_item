25f4b54e02d253e2c8d0400c2cc94b67595f29fd
your name <abc@gmail.com>
2023-01-19 13:41:33 +0700

    update

--- login_register/main.js
+++ login_register/main.js
@@ -105,7 +105,7 @@ function checkValueInput() {
         );
         SuccessToast.render();
         setTimeout(() => {
-          window.location = "/pageMarket.html";
+          window.location = "/login_register/pageMarket.html";
         }, 800);
       } else if (
         inputTextLogin.value !== e.userName &&
