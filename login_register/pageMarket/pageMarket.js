bbacc8bb7fa1aef65e8b1f6228ee1208600e7384
your name <abc@gmail.com>
2023-01-30 21:02:33 +0700

    Update pageMarket.js

--- login_register/pageMarket/pageMarket.js
+++ login_register/pageMarket/pageMarket.js
@@ -372,7 +372,7 @@ class addItemTab {
       <div class="itemInfo-rati"> ${this.info_ratiry}</div>
   
     <img
-      src="/Market_item/login_register/pageMarket/${this.img}"
+      src="../pageMarket/${this.img}"
       alt=""
       class="itemImg"
       title="${this.info_ratiry}"
